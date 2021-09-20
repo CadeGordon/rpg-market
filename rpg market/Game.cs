5a84542ef7696d3e58191e8f8b63780aba6bef62
Cade Gordon <cadegordon093@gmail.com>
2021-09-20 16:57:25 -0500

    template set out for game class and shop & player class made

---
+++ rpg market/Game.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rpg_market
+{
+    class Game
+    {
+        private bool _gameOver;
+        private int _currentScene;
+
+       
+
+
+
+
+
+
+        public void Run()
+        {
+            Start();
+
+            while (!_gameOver)
+            {
+                update();
+            }
+
+            End();
+        }
+
+        private void Start()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+
+        private void update()
+        {
+
+        }
+
+        private void InitializeItems()
+        {
+
+
+        }
+
+        int GetInput(string description, params string[] options)
+        {
+            string input = "";
+            int inputRecieved = -1;
+
+            while (inputRecieved == -1)
+            {
+                //Print options
+                Console.WriteLine(description);
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + options[i]);
+                }
+                Console.WriteLine("> ");
+
+                //get input from player
+                input = Console.ReadLine();
+
+                //If the player typed an int...
+                if (int.TryParse(input, out inputRecieved))
+                {
+                    //...decrement the input and check if it's within the bounds of the array
+                    inputRecieved--;
+                    if (inputRecieved < 0 || inputRecieved >= options.Length)
+                    {
+                        //set input recieved to be the default value
+                        inputRecieved = -1;
+                        //display error message
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadKey(true);
+                    }
+                }
+                //if the player didnt type and int
+                else
+                {
+                    //Set input recieved to be the default value
+                    inputRecieved = -1;
+                    Console.WriteLine("inavlid input");
+                    Console.ReadKey(true);
+                }
+
+                Console.Clear();
+            }
+
+            return inputRecieved;
+        }
+
+
+    }
+}
---
+++ rpg market/Shop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rpg_market
+{
+    class Shop
+    {
+
+    }
+}
